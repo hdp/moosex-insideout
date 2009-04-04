d1beee3a579eab7fe3190ccf36e7f3453c709cfe
hdp <hdp@3efe9002-19ed-0310-8735-a98156148065>
2009-04-04 05:43:32 +0000

    dzil-ize
    git-svn-id: http://code2.0beta.co.uk/moose/svn/MooseX-InsideOut/trunk@7885 3efe9002-19ed-0310-8735-a98156148065

--- lib/MooseX/InsideOut.pm
+++ lib/MooseX/InsideOut.pm
@@ -2,12 +2,11 @@ use strict;
 use warnings;
 
 package MooseX::InsideOut;
+# ABSTRACT: inside-out objects with Moose
 
 use MooseX::InsideOut::Meta::Class;
 BEGIN { require Moose }
 use Carp;
-
-our $VERSION = '0.003';
 
 sub import {
   my $class = shift;
@@ -31,14 +30,6 @@ sub import {
 
 1;
 __END__
-
-=head1 NAME
-
-MooseX::InsideOut - inside-out objects with Moose
-
-=head1 VERSION
-
-Version 0.003
 
 =head1 SYNOPSIS
 
@@ -79,55 +70,5 @@ whose internals you either don't want to care about or aren't hash-based.
 =item * (your suggestions here)
 
 =back
-
-=head1 AUTHOR
-
-Hans Dieter Pearcey, C<< <hdp at pobox.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-moosex-insideout at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-InsideOut>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc MooseX::InsideOut
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-InsideOut>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MooseX-InsideOut>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MooseX-InsideOut>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MooseX-InsideOut>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Hans Dieter Pearcey.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
 
 =cut
